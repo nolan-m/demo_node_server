4cac625c25c04e20e8c3c82a4ba98546a07812ff
nolan-m <namiller2@gmail.com>
2016-01-07 19:53:58 -0800

    Add post route for creating comments

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser 	= require('body-parser');
 var mongoose 		= require('mongoose');
 var Comment 		= require('./app/models/comment');
 
-mongoose.connect('mongodb://node:node@apollo.modulusmongo.net:27017/yja6waNy');
+mongoose.connect('mongodb://localhost:27017/nodetest');
 
 // confgure app to use bodyParser()
 // this will let us get the data from a POST
@@ -16,12 +16,33 @@ var port = process.env.PORT || 8080;
 // ROUTES
 var router = express.Router();
 
+router.use(function(req, res, next) {
+	console.log('Something is happening.');
+	next();
+});
+
 router.get('/', function (req, res) {
 	res.json({ message: 'hello world'});
 });
 
-
 // additional routes added here
+
+
+router.route('/comments')
+			.post(function(req, res) {
+
+				var comment = new Comment();  // create new instance of Comment model
+				comment.name = req.body.name;  // set the comment name
+
+				// save the comment
+				comment.save(function(err) {
+					if (err) {
+						res.send(err);
+					}
+					res.json({ message: 'Comment created!'});
+				});
+
+			});
 
 
 // REGISTER ROUTES
